9d701133fdd9b261ac00d613d6732802536b9549
NobreMateus <40170572+NobreMateus@users.noreply.github.com>
2020-06-10 10:31:22 -0300

    pages e recipes model criadas. Storyboard de receitas criada

---
+++ ChallengePuttingAllTogether/Model/Recipe.swift
@@ -0,0 +1,9 @@
+//
+//  Recipe.swift
+//  ChallengePuttingAllTogether
+//
+//  Created by Mateus Nobre Ferreira on 10/06/20.
+//  Copyright © 2020 Lidiane Gomes Barbosa. All rights reserved.
+//
+
+import Foundation
