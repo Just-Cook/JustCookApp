18513656bb58c67de7924c6fd63bca76829de29f
Lidiane Gomes Barbosa <lidiane@br-ifce-11.local>
2020-06-10 14:39:05 -0300

    Feature/area-resumo-perfil

--- ChallengePuttingAllTogether/Model/PerfilModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  PerfilModel.swift
-//  ChallengePuttingAllTogether
-//
-//  Created by Lidiane Gomes Barbosa on 10/06/20.
-//  Copyright © 2020 Lidiane Gomes Barbosa. All rights reserved.
-//
-
-import Foundation
