da91a2c7bd7b7f58a3fe9422fdcee41b452b9e0d
NobreMateus <40170572+NobreMateus@users.noreply.github.com>
2020-06-10 15:52:27 -0300

    Codigo de criacao de paginas de receita dinamicamente foi criado

--- RecipePageViewController.swift
+++ RecipePageViewController.swift
@@ -11,41 +11,33 @@ import UIKit
 class RecipePageViewController: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
     
     
-    let page1 = ViewController()
-    let page2 = ViewController()
+    var pageViewControllers:[UIViewController] = []
     
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        
-        if(viewController.view.backgroundColor == .red) {
-            return page1
-        }else {
-            return page2
-        }
-        
-    }
-    
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        if(viewController.view.backgroundColor == .red) {
-            return page1
-        }else {
-            return page2
-        }
-    }
+    let pages:[Page] = [
+        Page(text: "Pagina 1 da receita"),
+        Page(text: "Pagina 2 da receita"),
+        Page(text: "Pagina 3 da receita"),
+        Page(text: "Pagina 4 da receita"),
+        Page(text: "Pagina 5 da receita"),
+        Page(text: "Pagina 6 da receita"),
+        Page(text: "Pagina 7 da receita"),
+        Page(text: "Pagina 8 da receita")
+    ]
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        page1.view = UIView()
-        page1.view.backgroundColor = .blue
-        
-        page2.view = UIView()
-        page2.view.backgroundColor = .red
-        
         self.dataSource = self
         self.delegate = self
         
-        setViewControllers([page1], direction: .forward, animated: false, completion: nil)
+        createViewControllers()
+        
+        if let firstPageViewController = pageViewControllers.first{
+            setViewControllers([firstPageViewController], direction: .forward, animated: false, completion: nil)
+        }
+        
+        
         configurePagination()
     }
     
@@ -60,8 +52,57 @@ class RecipePageViewController: UIPageViewController, UIPageViewControllerDelega
         topView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
         topView.heightAnchor.constraint(equalToConstant: 78).isActive = true
         topView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+    }
+    
+    
+    func createViewControllers(){
         
-       
+        for page in pages {
+            
+            let viewController = UIViewController()
+            viewController.view.backgroundColor = .white
+            let textView = UITextView()
+            textView.isEditable = false
+            textView.text = page.text
+            viewController.view.addSubview(textView)
+            
+            textView.translatesAutoresizingMaskIntoConstraints = false
+            textView.leadingAnchor.constraint(equalTo: viewController.view.leadingAnchor, constant: 0).isActive = true
+            textView.trailingAnchor.constraint(equalTo: viewController.view.trailingAnchor, constant: 0).isActive = true
+            textView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+            textView.topAnchor.constraint(equalTo: viewController.view.topAnchor, constant: 94).isActive = true
+            
+            pageViewControllers.append(viewController)
+        }
+    }
+    
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        guard let currentIndex = pageViewControllers.firstIndex(of: viewController) else { return nil }
+        
+        let beforeIndex = currentIndex - 1
+        
+        guard beforeIndex >= 0 else {
+            return nil
+        }
+        
+        return pageViewControllers[beforeIndex]
+    }
+    
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        guard let currentIndex = pageViewControllers.firstIndex(of: viewController) else { return nil }
+        
+        let afterIndex = currentIndex + 1
+        
+        guard afterIndex <= pageViewControllers.count - 1 else {
+            return nil
+        }
+        
+        return pageViewControllers[afterIndex]
+        
     }
     
 }
