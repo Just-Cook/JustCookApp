8104eb1a105e687b2abc5b02ed5cfa2d0e2131e1
Lidiane Gomes Barbosa <lidiane@br-ifce-11.local>
2020-06-10 14:46:01 -0300

    Revert "Feature/area-resumo-perfil"
    This reverts commit 18513656bb58c67de7924c6fd63bca76829de29f.

---
+++ ChallengePuttingAllTogether/Model/PerfilModel.swift
@@ -0,0 +1,9 @@
+//
+//  PerfilModel.swift
+//  ChallengePuttingAllTogether
+//
+//  Created by Lidiane Gomes Barbosa on 10/06/20.
+//  Copyright © 2020 Lidiane Gomes Barbosa. All rights reserved.
+//
+
+import Foundation
