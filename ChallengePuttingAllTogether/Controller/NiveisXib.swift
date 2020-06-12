7fdca725b69d5dd1b050655aadcaff6b00492c8c
Lidiane Gomes Barbosa <lidiane@br-ifce-11.local>
2020-06-12 17:56:21 -0300

    xib de niveis

--- ChallengePuttingAllTogether/Controller/NiveisXib.swift
+++ ChallengePuttingAllTogether/Controller/NiveisXib.swift
@@ -10,6 +10,50 @@ import UIKit
 
 class NiveisXib: UIView {
 
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var titulo: UILabel!
+    @IBOutlet weak var subtitulo: UILabel!
+    
+     override func draw(_ rect: CGRect) {
+            self.layer.cornerRadius = 8
+            self.layer.backgroundColor = CGColor.init(srgbRed: 36/255, green: 153/255, blue: 242/255, alpha: 1)
+       }
+
+       
+       var contentView: UIView?
+       var xibName = "Niveis"
+         
+         required init?(coder aDecoder:NSCoder) {
+               super.init(coder: aDecoder)
+            
+               setupXib()
+               
+           }
+         
+         private func setupXib(){
+             guard let view = loadViewFromNib()
+                 else{ fatalError("Wrong xib name")}
+             view.frame = self.bounds
+             self.addSubview(view)
+             contentView = view
+             configureXibNivel()
+         }
+         
+         private func loadViewFromNib() -> UIView?{
+             let bundle = Bundle(for: type(of: self))
+             let nib = UINib(nibName: xibName, bundle: bundle)
+             return nib.instantiate(withOwner: self, options: nil).first as? UIView
+         }
+         
+         
+         func configureXibNivel(){
+           image.clipsToBounds = true
+          image.layer.cornerRadius = 8
+
+         }
+    
+
+    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
