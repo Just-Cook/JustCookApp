ee69d238cb2be2a553feb5f2b39bd336d94e7287
Lidiane Gomes Barbosa <lidiane@br-ifce-11.local>
2020-06-12 17:56:00 -0300

    xib de niveis

---
+++ ChallengePuttingAllTogether/Controller/NiveisXib.swift
@@ -0,0 +1,21 @@
+//
+//  NiveisXib.swift
+//  ChallengePuttingAllTogether
+//
+//  Created by Lidiane Gomes Barbosa on 12/06/20.
+//  Copyright © 2020 Lidiane Gomes Barbosa. All rights reserved.
+//
+
+import UIKit
+
+class NiveisXib: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
