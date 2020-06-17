4506f3d2770690d72962801cc3de5155c797452d
Maria Cecilia <ceciliasoares0@gmail.com>
2020-06-17 10:11:45 -0300

    Criado arquivo de cores

---
+++ ChallengePuttingAllTogether/Model/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  ChallengePuttingAllTogether
+//
+//  Created by Cecilia Soares on 17/06/20.
+//  Copyright © 2020 Lidiane Gomes Barbosa. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let orangeColor = #colorLiteral(red: 0.9725490196, green: 0.6352941176, blue: 0.2039215686, alpha: 1)
+    static let blueColor = #colorLiteral(red: 0.2156862745, green: 0.6039215686, blue: 0.8980392157, alpha: 1)
+    static let greenColor = #colorLiteral(red: 0.4392156863, green: 0.6549019608, blue: 0.09411764706, alpha: 1)
+    static let redColor = #colorLiteral(red: 0.8156862745, green: 0.2352941176, blue: 0.2352941176, alpha: 1)
+}
