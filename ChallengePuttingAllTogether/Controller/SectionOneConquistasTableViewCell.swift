3ac38e4573d3f0b6d63e6fde208f9e8163034f84
LidyIfce <lidianegomesbarbosa@gmail.com>
2020-06-25 14:10:58 -0300

    onsquistas secition one

---
+++ ChallengePuttingAllTogether/Controller/SectionOneConquistasTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SectionOneConquistasTableViewCell.swift
+//  ChallengePuttingAllTogether
+//
+//  Created by Lidiane Gomes Barbosa on 25/06/20.
+//  Copyright © 2020 Lidiane Gomes Barbosa. All rights reserved.
+//
+
+import UIKit
+
+class SectionOneConquistasTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
